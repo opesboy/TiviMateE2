c8b8b08c5e2b79d90b7f09025fcc6c82c424d6fa
opesboy <opesboy@gmail.com>
2026-08-29 22:36:13 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,17 @@
 set -e
 
 BASE="https://raw.githubusercontent.com/opesboy/TiviMateE2/main"
-IPK="enigma2-plugin-extensions-tivimatee2_1.0.08_all.ipk"
-DEB="enigma2-plugin-extensions-tivimatee2_1.0.08_all.deb"
+IPK="enigma2-plugin-extensions-tivimatee2_1.0.09_all.ipk"
+DEB="enigma2-plugin-extensions-tivimatee2_1.0.09_all.deb"
 
 if command -v opkg >/dev/null 2>&1; then
     wget -qO "/tmp/$IPK" "$BASE/$IPK"
     opkg install --force-reinstall "/tmp/$IPK"
+
 elif command -v dpkg >/dev/null 2>&1; then
     wget -qO "/tmp/$DEB" "$BASE/$DEB"
     dpkg -i "/tmp/$DEB"
+
 else
     echo "Unsupported system: opkg/dpkg not found"
     exit 1
